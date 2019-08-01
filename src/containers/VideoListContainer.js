4f3a4988f18e14274aef683f2477116f81a66fa5
inyhsu <isingridsu@gmail.com>
2019-07-31 20:38:43 -0700

    Completed up to container

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,7 +2,19 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
-var VideoListContainer = () => {};
+var VideoListContainer = connect(mapStateToProps, mapDispatchToProps)(VideoList);
+
+const mapStateToProps = (state) =>{
+  return {
+    videos: state.videos,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleVideoListEntryTitleClick: (video) => dispatch(changeVideo(video))
+  };
+};
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,17 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
-  //TODO: define a reducer for the currentVideo field of our state.
+var currentVideoReducer = (state=null, action) => {
+  // TODO: define a reducer for the currentVideo field of our state.
+  if (action.type === "CHANGE_VIDEO") {
+    return action.video;
+  }
+  return state
 };
+// switch(action.type) {
+//   case "CHANGE_VIDEO":
+//   return state=action.video
+//   default:
+//     return state
+// }
 
 export default currentVideoReducer;
